23d389f466a4d42886c1bb9cc87fd1fde20b785f
NathanCHEFF <54266398+NathanCHEFF@users.noreply.github.com>
2020-09-12 19:04:32 +0300

    13

--- master/main.js
+++ master/main.js
@@ -1,1 +1,13 @@
-alert(12)
+function craeter(){
+
+  // Добавляем только что созданый элемент в дерево DOM
+  var elem = document.createElement("div");
+  elem.id = 'main';
+
+  
+  document.body.appendChild(elem);
+}
+
+function addNewElement(name){
+  return document.createElement(name);
+}
