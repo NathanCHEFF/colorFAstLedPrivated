ccf4138c8dc564a2de11b80491e46fbbccdfee16
NathanCHEFF <54266398+NathanCHEFF@users.noreply.github.com>
2020-09-12 19:23:06 +0300

    Update main.js

--- master/main.js
+++ master/main.js
@@ -1,15 +1,4 @@
-function creater(){
-  // Добавляем только что созданый элемент в дерево DOM
-  var elem = document.createElement("div");
-  elem.id = 'main';
 
-
-  document.body.appendChild(elem);
-}
-
-function addNewElement(name){
-  return document.createElement(name);
-}
 
 colorPicker.addEventListener("input", updateFirst, false);
 colorPicker.addEventListener("change", watchColorPicker, false);
@@ -19,5 +8,3 @@ function watchColorPicker(event) {
     alert(event.target.value);
 
 }
-
-document.body.onload = 'creater';
