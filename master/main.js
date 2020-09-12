5b7c443d82de8184160f123b1989392fc503d984
NathanCHEFF <54266398+NathanCHEFF@users.noreply.github.com>
2020-09-12 19:28:52 +0300

    Update main.js

--- master/main.js
+++ master/main.js
@@ -1,10 +1,5 @@
 
+document.getElementById("selectColor").addEventListener('change', (e) => {
+  alert(e.target.value);
 
-colorPicker.addEventListener("input", updateFirst, false);
-colorPicker.addEventListener("change", watchColorPicker, false);
-
-function watchColorPicker(event) {
-
-    alert(event.target.value);
-
-}
+})
